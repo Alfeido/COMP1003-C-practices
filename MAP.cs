a36dd419986b3c9d520336b5354fc82ea85146e2
Alfeido <144791923+Alfeido@users.noreply.github.com>
2025-04-10 14:21:20 +0100

    Add files via upload

---
+++ MAP.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<string, double> employeePay = new Dictionary<string, double>();
+
+        // (ii) Add Homer and Monty
+        employeePay["Homer"] = 20000;
+        employeePay["Monty"] = 500000;
+
+        employeePay.Remove("Homer");
+
+        if (employeePay.ContainsKey("Carl"))
+        {
+            employeePay["Carl"] -= 1000;
+        }
+
+        foreach (var key in employeePay.Keys.ToList())
+        {
+            employeePay[key] = employeePay[key] * 0.1;
+        }
+    }
+}
